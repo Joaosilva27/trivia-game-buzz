a1675882778204228210f6d17e3a63605410bcb3
Joaosilva27 <joaosilva7875@gmail.com>
2025-04-21 17:38:44 +0200

    new style for StartGame component
    * replaced LayingBuzz image with a image of the Buzz! host standing
    
    * removed span containing the string "Buzz! Trivia Game"
    
    * minor tailwind css changes
    
    * added Buzz! logo

--- src/components/StartGame.tsx
+++ src/components/StartGame.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import LayingBuzzImage from "../images/layingBuzz.png";
+import BuzzLogo from "../images/BuzzLogo.png";
+import BuzzHostImage from "../images/BuzzHost.png";
 import GithubIcon from "../images/githubIcon.png";
 
 interface StartGameProps {
@@ -9,19 +10,17 @@ interface StartGameProps {
 const StartGame: React.FC<StartGameProps> = ({ onStart }) => {
   return (
     <div className='flex items-center justify-center min-h-screen bg-gradient-to-br from-slate-800 to-slate-900'>
-      <div className='w-full max-w-lg px-8 py-10 text-center'>
-        <h1 className='mb-6 text-4xl font-bold text-white'>
-          BUZZ!
-          <span className='block mt-2 text-2xl font-medium text-amber-400'>TRIVIA GAME</span>
-        </h1>
-
-        <div className='relative mb-8'>
-          <img src={LayingBuzzImage} alt='Buzz Character' className='relative z-10 mx-auto h-80' />
+      <div className='w-full flex justify-center items-center flex-col max-w-lg px-8 py-10 text-center'>
+        <div className='flex'>
+          <div className='relative mb-8'>
+            <img src={BuzzHostImage} alt='Buzz Character' className='relative z-10 mx-auto h-80' />
+          </div>
+          <img src={BuzzLogo} className='w-26 h-26 ml-4' />
         </div>
 
         <button
           onClick={onStart}
-          className='px-12 py-4 text-xl font-bold text-slate-900 transition-all duration-300 bg-amber-400 rounded-full hover:bg-amber-300 hover:shadow-lg hover:shadow-amber-400/30 transform hover:-translate-y-1'
+          className='px-12 py-4 w-fit text-xl font-bold text-slate-900 transition-all duration-300 bg-amber-400 rounded-full hover:bg-amber-300 hover:shadow-lg hover:shadow-amber-400/30 transform hover:-translate-y-1'
         >
           PLAY NOW
         </button>
